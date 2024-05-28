194564e031f918c25ec30580e6dbd307da31c8ca
yashsoni1234567 <yashd1soni@gmail.com>
2024-05-28 07:31:27 +0000

    datatypes

---
+++ .devcontainer/yash basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)// we're using node js not browser
+
+console.log(3+3); 
+//code readability should be high
+
+console.log("yash");
+
+//datatypes
+let name = "yash"
+let age = 18
+let isLoggedIn = true
+let state = null
+let city;
+
+//number => 2 to power 53
+// bigint 
+//string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined =>
+// symbol => unique
+// object 
+
+console.log(typeof undefined); // op (undefined)
+console.log(typeof null); // op (object)
