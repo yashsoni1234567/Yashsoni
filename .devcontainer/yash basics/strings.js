5f92f2f0c0efffc1423d75e5f0f1f39c4faa947e
yashsoni1234567 <yashd1soni@gmail.com>
2024-05-30 14:04:57 +0000

    strings

---
+++ .devcontainer/yash basics/strings.js
@@ -0,0 +1,33 @@
+const name =" yash "
+const repocount = 50
+//console.log(name+repocount);
+
+//console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+
+const gamename = new String("blaezi-pub-com");
+//console.log(gamename[0]);
+//console.log(gamename.__proto__);
+
+//console.log(gamename.length);
+//console.log(gamename.toUpperCase());
+//console.log(gamename.charAt(4));
+// reverse
+//console.log(gamename.indexOf('l'));
+
+const newString = gamename.substring(0, 3)
+//console.log(newString);
+
+const anotherString = gamename.slice(-8, 4)
+//console.log(anotherString);
+
+const newStringOne = "    yash    "
+//console.log(newStringOne);
+//console.log(newStringOne.trim());
+
+const url = "https://yash.com/hitesh%30soni"
+//console.log(url.replace('%30', '-'));
+//console.log(url.includes('yash'));
+
+console.log(gamename.split('-'));
+
