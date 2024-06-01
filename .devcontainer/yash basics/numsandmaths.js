a22f3be2df3b91285d58b7ed49d488f0101d56f8
yashsoni1234567 <yashd1soni@gmail.com>
2024-06-01 07:43:09 +0000

    math and num

---
+++ .devcontainer/yash basics/numsandmaths.js
@@ -0,0 +1,35 @@
+const score = 400
+
+const balance = new Number(100)
+//console.log(score);
+//console.log(balance);
+
+//console.log(balance.toString());
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const otherNumber = 1123.8966
+//console.log(otherNumber.toPrecision(5));
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++ Maths ++++++++++++++++++++++//
+
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.6));
+//console.log(math.ceil(4.2));
+//console.log(Math.floor(4.9));
+//console.log(Math.min(4,5,6,7,8,9,2));
+//console.log(Math.max(4,5,2,6,3,8,3,9));
+
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log( Math.floor(Math.random() * (max - min + 1) + min));
